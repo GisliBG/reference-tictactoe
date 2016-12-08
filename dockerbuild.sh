980941cc8737f91858060e6a602bc359d4f6dfcc
Gísli Böðvar Guðmundsson <gisligudmundss@gmail.com>
2016-12-08 15:28:42 +0000

    still trying to deploy to aws

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -40,8 +40,8 @@ cat > ./build/public/version.html <<_EOF_
 _EOF_
 
 #Move neccesery files to build folder so they will be available on other state machines
-cp ~/gislibg-key-pair.pem /var/lib/jenkins/workspace/Commit\ Stage\ Job//Dockerfile ./build/
-cp ~/gislibg-key-pair.pem /var/lib/jenkins/workspace/Commit\ Stage\ Job//migratescript.sh ./build/
+cp ./Dockerfile ./build/
+cp ./migratescript.sh ./build/
 
 #Saving this commit id on a enviorment file to identify with docker build 
 cat > ./.env <<_EOF_
@@ -70,8 +70,8 @@ fi
 
 echo "sending docker-compse & env folder to aws"
 #Moving neccesery files to aws so it can run
-scp -i ~/gislibg-key-pair.pem /var/lib/jenkins/workspace/Commit\ Stage\ Job/docker-compose.yml  ec2-user@35.160.42.253:~/.
-scp -i ~/gislibg-key-pair.pem /var/lib/jenkins/workspace/Commit\ Stage\ Job/.env  ec2-user@35.160.42.253:~/.
+scp -i ~/gislibg-key-pair.pem ~/workspace/Commit\ Stage\ Job/docker-compose.yml  ec2-user@35.160.42.253:~/.
+scp -i ~/gislibg-key-pair.pem ~/workspace/Commit\ Stage\ Job/.env  ec2-user@35.160.42.253:~/.
 
 echo "restarting docker build on aws server(work in progress)"
 #ssh -i ~/Downloads/gislibg-key-pair.pem ec2-user@35.160.42.253 < ../provisioning/provision.sh
