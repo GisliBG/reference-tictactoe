053383dccf39f26141f5a1c6ce1149aabefc4275
Gísli Böðvar Guðmundsson <gisligudmundss@gmail.com>
2016-12-08 15:33:05 +0000

    Trying to deploy
    found out I was in wrong directory, hope this will work

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -13,6 +13,7 @@ fi
 # Remove .git from url in order to get https link to repo (assumes https url for GitHub)
 export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
 npm install --silent && cd client && npm install --silent
+cd ..
 echo Building app
 npm run build
 
