fbb299a7ab04674f8936d0c52f8154468c235292
Gísli Böðvar Guðmundsson <gisligudmundss@gmail.com>
2016-12-09 16:13:01 +0000

    minor bug fix
    wrong path to provisioning folder.

--- deployscript.sh
+++ deployscript.sh
@@ -5,5 +5,5 @@ scp -i ~/gislibg-key-pair.pem ~/workspace/Commit\ Stage\ Job/.env  ec2-user@35.1
 
 echo "restarting docker build on aws server(work in progress)"
 #ssh -i ~/Downloads/gislibg-key-pair.pem ec2-user@35.160.42.253 < ../provisioning/provision.sh
-ssh -i ~/gislibg-key-pair.pem ec2-user@35.160.42.253 < ../provisioning/provision.sh
+ssh -i ~/gislibg-key-pair.pem ec2-user@35.160.42.253 < /provisioning/provision.sh
 echo "Done"
