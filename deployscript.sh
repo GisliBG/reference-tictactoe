163f7ad3a5d5c998f041e963b9ba4f2f359e77b8
Gísli Böðvar Guðmundsson <gisligudmundss@gmail.com>
2016-12-10 20:29:06 +0000

    original fetch

--- deployscript.sh
+++ deployscript.sh
@@ -3,7 +3,7 @@ echo "sending docker-compse & env folder to aws"
 scp -i ~/gislibg-key-pair.pem ~/workspace/Commit\ Stage\ Job/docker-compose.yml  ec2-user@35.160.42.253:~/.
 scp -i ~/gislibg-key-pair.pem ~/workspace/Commit\ Stage\ Job/.env  ec2-user@35.160.42.253:~/.
 
-echo "restarting docker build on aws server(work in progress)"
+echo "restarting docker build on aws server"
 #ssh -i ~/Downloads/gislibg-key-pair.pem ec2-user@35.160.42.253 < ../provisioning/provision.sh
 ssh -i ~/gislibg-key-pair.pem ec2-user@35.160.42.253 < ./provisioning/provision.sh
 echo "Done"
