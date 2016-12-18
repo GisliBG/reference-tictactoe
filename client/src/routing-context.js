e016a832f4525e981e815a4639f383af5fb89703
Gísli Böðvar Guðmundsson <gisligudmundss@gmail.com>
2016-12-18 14:18:10 +0000

    trying to get jenkins to work

--- client/src/routing-context.js
+++ client/src/routing-context.js
@@ -15,7 +15,7 @@ function routingContext(injected){
     const environment = injected('env');
     var socketURI;
     if(environment==='development' || environment==='test'){
-        socketURI='http://localhost:8080'
+        socketURI='http://82.221.49.171:8080/'
     } else {
         socketURI='http://35.160.42.253/'
     }
