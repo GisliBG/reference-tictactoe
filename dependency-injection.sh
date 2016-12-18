7424a765d6ae47efed175dea0632e614ba12fd40
Gísli Böðvar Guðmundsson <gisligudmundss@gmail.com>
2016-12-18 16:04:32 +0000

    fixing the jenkins pipeline

--- dependency-injection.sh
+++ dependency-injection.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+npm install nodemon
+npm install create-react-app
+
+echo "restarting postgres image"
+docker stop pg2
+docker rm pg2
+docker run -p 5432:5432 --name pg2 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+
 #Remove previous build 
 echo Cleaning...
 rm -rf ./build
@@ -7,6 +15,8 @@ rm -rf ./build
 echo "instaling all dependencies"
 npm install --silent && cd client && npm install --silent
 cd ..
+
+npm run migratedb-dev
 
 echo "running tests"
 ./startTests.sh
